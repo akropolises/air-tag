79ca93ee4f32c8fa8d2a717861ca1a2faf07c2f9
akropolises <onk.10781@gmail.com>
2022-06-23 12:38:17 +0900

    replace 2./ -> 2.7

--- dataformat.py
+++ dataformat.py
@@ -1,7 +1,6 @@
 with open("data.csv", "r", encoding= "utf_8") as f:
     all = f.readlines()
 with open("data.csv", "w", encoding= "utf_8") as g:
-    for p in all[:1]:
-        print(p)
+    for p in all:
         p = p.replace("2./","2.7")
         g.write(p)
